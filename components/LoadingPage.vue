1b535cdf99de892236b4b52d91673b221aa8e3de
Atyson Jaime <atysonjaime@gmail.com>
2022-12-10 15:26:26 -0300

    :zap: Cria página de Loading Inicial

---
+++ components/LoadingPage.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    .loading-page
+        .content
+            .image
+                b-skeleton(height="12.5rem" width='14.813rem')
+            .info
+                b-skeleton(height="1.118rem" width='14.813rem')
+            .dados
+                b-skeleton(height="2.5rem" width='9rem')
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.loading-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 6rem;
+
+  .info {
+    margin: 2rem 0 1rem;
+  }
+
+  .dados {
+    display: flex;
+    justify-content: center;
+
+    .b-skeleton {
+      width: auto !important;
+    }
+  }
+}
+</style>

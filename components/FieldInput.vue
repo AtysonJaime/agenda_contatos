a64dda46759314e2b9ec63c0dc0d25b73d808c34
Atyson Jaime <atysonjaime@gmail.com>
2022-12-10 15:02:05 -0300

    :sparkles: Adiciona modal de Adicionar Contato

---
+++ components/FieldInput.vue
@@ -0,0 +1,104 @@
+<template lang="pug">
+.field
+    label.label {{label}}
+    .control(:class='{"has-icons-right" : error !== ""}')
+        input.input(
+            v-if='mascara === ""'
+            v-model='value'
+            :type='inputType'
+            @keyup='input'
+            :class='{"is-danger" : error !== ""}'
+            type='text'
+            autocomplete='on'
+            :required='requiredInput'
+        )
+        input.input(
+            v-else
+            v-model='value'
+            :type='inputType'
+            @keyup='input'
+            :class='{"is-danger" : error !== ""}'
+            type='text'
+            autocomplete='on'
+            required
+            v-mask='mascara'
+        )
+        span.icon.is-right.has-text-danger(v-show='error !== ""')
+            i.mdi.mdi-alert-circle.mdi-24px
+    p.help.is-danger(v-show='error !== ""') {{error}}
+</template>
+
+<script>
+import { mask } from 'vue-the-mask'
+
+export default {
+  directives: { mask },
+  props: {
+    error: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    label: {
+      type: String,
+      required: true,
+      default: 'Default',
+    },
+    inputType: {
+      type: String,
+      required: true,
+      default: 'text',
+    },
+    mascara: {
+      type: [Array, Object, String],
+      required: false,
+      default: '',
+    },
+    requiredInput: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      value: '',
+    }
+  },
+
+  methods: {
+    input() {
+      this.$emit('vmodel', [this.label.toLowerCase(), this.value])
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import './assets/sass/variable.scss';
+.field {
+  label {
+    color: $dark;
+    font-size: 0.875rem;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    display: flex;
+  }
+  input {
+    height: 2rem;
+    border-color: #c0c3d2;
+    background-color: $white-two;
+
+    &.is-danger {
+      border-color: #f14668;
+    }
+  }
+  .icon {
+    height: 2em;
+  }
+}
+</style>
--- components/ModalCriarContato.vue
+++ components/ModalCriarContato.vue
@@ -1,19 +1,106 @@
 <template lang="pug">
 .modal-cria-contato
-    button.button(type='button' @click='isModalCriaContatoActive = true')
+    button.button.openModal(type='button' @click='isModalCriaContatoActive = true')
         span.icon
             i.mdi.mdi-plus
         span Criar contato
-    b-modal(v-model='isModalCriaContatoActive')
-        p Sou um modal
+    b-modal(v-model='isModalCriaContatoActive' @close='clearAll')
+        .card
+          .card-header
+            p.card-header.title Criar novo contato
+          .card-content
+            FieldInput(label='Nome' inputType='text' :error='error.nome' @vmodel='changeValue')
+            FieldInput(label='E-mail' inputType='email' :error='error.email' @vmodel='changeValue')
+            FieldInput.inputTelefone(label='Telefone' inputType='tel' :error='error.telefone' @vmodel='changeValue' :mascara="['(##) #####-####','(##) ####-####']")
+          .card-footer
+            button.button.button-link(type='button' @click='closeModal()') Cancelar
+            button.button.button-save(type='button' @click='saveContato()' :disabled='isDisabled') Salvar
 </template>
 
 <script>
+import FieldInput from './FieldInput.vue'
 export default {
   data() {
     return {
       isModalCriaContatoActive: false,
+      contato: {
+        nome: '',
+        email: '',
+        telefone: '',
+      },
+      error: {
+        nome: '',
+        email: '',
+        telefone: '',
+      },
     }
+  },
+
+  computed: {
+    isDisabled() {
+      return (
+        this.contato.nome === '' &&
+        this.contato.email === '' &&
+        this.contato.telefone === ''
+      )
+    },
+  },
+
+  component: {
+    FieldInput,
+  },
+
+  methods: {
+    clearAll() {
+      this.contato.name = ''
+      this.contato.email = ''
+      this.contato.telefone = ''
+      this.error.name = ''
+      this.error.email = ''
+      this.error.telefone = ''
+    },
+
+    closeModal() {
+      this.clearAll()
+      this.isModalCriaContatoActive = false
+    },
+
+    changeValue(e) {
+      this.contato[e[0].replace('-', '')] = e[1]
+    },
+
+    validaInputEmail() {
+      const RegEx = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/
+      if (!RegEx.test(this.contato.email)) {
+        this.error.email = 'Adicione um email valido'
+      } else {
+        this.error.email = ''
+      }
+    },
+
+    validaInputTelefone() {
+      if (this.contato.telefone.length < 14) {
+        this.error.telefone = 'Adicione um telefone valido'
+      } else {
+        this.error.telefone = ''
+      }
+    },
+
+    async saveContato() {
+      if (this.contato.email !== '') {
+        this.validaInputEmail()
+      }
+      if (this.contato.telefone !== '') {
+        this.validaInputTelefone()
+      }
+      if (
+        this.error.nome === '' &&
+        this.error.email === '' &&
+        this.error.telefone === ''
+      )
+        await this.$store.dispatch('contatos/addContato', this.contato)
+      this.closeModal()
+    },
   },
 }
 </script>
@@ -25,26 +112,16 @@ export default {
   display: flex;
   justify-content: center;
 
-  button {
+  .openModal {
     display: flex;
     justify-content: center;
-    cursor: pointer;
     align-items: center;
-    font-size: 0.875rem;
-    font-weight: 500;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
     color: $light-red;
     width: 9rem;
     height: 2.5rem;
     background-color: $light-yellowish-green;
     border-radius: 20px;
     border: 0;
-    box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.16),
-      0 0 0 0.5px rgba(0, 0, 0, 0.08), inset 0 0 0 0.5px rgba(0, 0, 0, 0.08),
-      0 2px 4px 0.5px rgba(0, 0, 0, 0.16);
 
     .icon {
       margin-right: 0.3125rem;
@@ -56,6 +133,81 @@ export default {
     &:hover,
     &:focus {
       background-color: $light-yellowish-green-hover;
+    }
+  }
+
+  .card {
+    border-radius: 1rem;
+    width: 27rem;
+    background-color: $white-two;
+
+    .card-header {
+      padding: 1rem;
+      box-shadow: none;
+      border-bottom: 0.0625rem solid #c0c3d2;
+      p {
+        padding: 0;
+        border: 0;
+        box-shadow: none;
+        font-weight: normal;
+        color: $dark;
+        font-size: 1rem;
+      }
+    }
+
+    .card-content {
+      padding: 1.5rem;
+
+      .inputTelefone {
+        width: 55%;
+      }
+
+      .field {
+        display: flex;
+        flex-direction: column;
+        text-align: start;
+      }
+
+      label {
+        text-align: start !important;
+      }
+    }
+
+    .card-footer {
+      padding: 1rem;
+      border-top: 0.0625rem solid #c0c3d2;
+      box-shadow: none;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+
+      .button-link {
+        border: 0;
+        box-shadow: 0;
+        padding: 0;
+        background-color: $white-two;
+        color: $light-red;
+        margin-right: 1rem;
+
+        &:hover,
+        &:focus {
+          text-decoration: underline;
+        }
+      }
+
+      .button-save {
+        border-radius: 1rem;
+        padding: 0.5rem 1rem;
+        margin: 0;
+        height: 2rem;
+        background-color: $light-red;
+        color: $white-two;
+
+        &:hover,
+        &:focus {
+          background-color: $light-red-hover;
+        }
+      }
     }
   }
 }

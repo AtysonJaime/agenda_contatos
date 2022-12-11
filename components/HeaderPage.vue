654230407341a1c8bb19f5c34b4c39d882f359b1
Atyson Jaime <atysonjaime@gmail.com>
2022-12-10 23:59:33 -0300

    :art: Alteração do componente de imagem

--- components/HeaderPage.vue
+++ components/HeaderPage.vue
@@ -2,7 +2,7 @@
   nav.navbar.header(role='navigation' aria-label='main navigation')
       .navbar-brand
         a.navbar-item(href='/')
-          img(:src='imagem' alt='Logo')
+          b-image(:src='imagem' alt='Logo')
         ModalCriarContato(v-if='agenda.length > 0')
         .navbar-busca
           .field
